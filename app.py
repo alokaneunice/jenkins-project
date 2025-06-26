3446dc2335f16c73b357bf3437b0d6a3a422784d
alokaneunice <alokaneunice@outlook.com>
2025-06-26 21:54:41 +0100

    commit made

--- app.py
+++ app.py
@@ -4,14 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def student():
-return render_template('student.html')
+ return render_template('student.html')
 
 
 @app.route('/from flask import Flask, render_template, requestresult', methods=['POST', 'GET'])
 def result():
-if request.method == 'POST':
-result = request.form
-return render_template("result.html", result=result)
+ if request.method == 'POST':
+  result = request.form
+ return render_template("result.html", result=result)
 
 
 # return render_template("registrationform.html")
@@ -20,4 +20,4 @@ if __name__ == '__main__':
 # run() method of Flask class runs the application
 # on the local development server.
 # app.run(debug=True, port=5002)
-app.run(host='0.0.0.0', port=5007)
+ app.run(host='0.0.0.0', port=5007)
